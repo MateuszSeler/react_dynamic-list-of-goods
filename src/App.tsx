f4c7e26b5c16cbfb81de2e5cfad1b5b6d2c36d76
Mateusz Seler <mateusz.seler@gmail.com>
2026-03-04 18:12:44 +0100

    implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum Mode {
+  'NON',
+  'FIRST_FIVE',
+  'RED',
+}
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+export const App: React.FC = () => {
+  const [, setMode] = useState<Mode>(Mode.NON);
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const strategy = {
+    [Mode.NON]: getAll,
+    [Mode.FIRST_FIVE]: get5First,
+    [Mode.RED]: getRedGoods,
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleClick = (change: Mode) => {
+    setMode(change);
 
-    <GoodsList goods={[]} />
-  </div>
-);
+    strategy[change]().then(setGoods);
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => handleClick(Mode.NON)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => handleClick(Mode.FIRST_FIVE)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => handleClick(Mode.RED)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
